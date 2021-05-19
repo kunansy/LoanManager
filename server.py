0dac8d331745a1e207dce2b46f1b9ebac1d5e9fb
kunansy <kolobov.kirill@list.ru>
2021-05-19 16:00:09 +0300

    base for db_api added

--- server.py
+++ server.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
+import os
+
+from sanic import Sanic, Request, HTTPResponse, response
+from sanic_openapi import swagger_blueprint
+from pydantic import BaseModel, constr
+
+from src import db_api
+
+
+app = Sanic(__name__)
+app.blueprint(swagger_blueprint)
+
+
+class Loan(BaseModel):
+    product_name: constr(strip_whitespace=True, min_length=1)
+
+
+@app.post('/loans')
+async def add_loan(request: Request) -> HTTPResponse:
+    pass
+
+
+@app.get('/')
+async def get_loan(request: Request,
+                   loan_id: int) -> HTTPResponse:
+    pass
+
 
 if __name__ == "__main__":
-    pass
+    app.run(
+        port=8081,
+        workers=os.cpu_count(),
+        access_log=False
+    )
